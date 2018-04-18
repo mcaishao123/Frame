fec6a97ed4da87d1ceb37c8ff7a8a4a96bf6ebf9
ivan <zhengmc123456@gmail.com>
2018-04-18 23:36:16 +0800

    modified

--- UserCenter/proguard-rules.pro
+++ UserCenter/proguard-rules.pro
@@ -19,7 +19,3 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
-
-#ARouter
--keep public class com.alibaba.android.arouter.routes.**{*;}
--keep class * implements com.alibaba.android.arouter.facade.template.ISyringe{*;}
--- UserCenter/src/main/java/com/qianzhihe/user/ui/activity/LoginActivity.java
+++ UserCenter/src/main/java/com/qianzhihe/user/ui/activity/LoginActivity.java
@@ -1,20 +1,21 @@
-package com.qianzhihe.xianggu;
+package com.qianzhihe.user.ui.activity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.qianzhihe.user.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
     }
+
 }
